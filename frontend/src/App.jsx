61edf55fb7d9c2b92629ff076ff5cffcef032d47
febtwenty <clemens.leopold@outlook.com>
2025-11-30 22:32:43 +0100

    Cleaned react frontend and added search functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [newArtist, setNewArtist] = useState('')
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Some Artdatabase</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h4>Find an artist:</h4>
+        <p>Artist name: <input value={newArtist} onChange={e => setNewArtist(e.target.value)}/></p>
+        <p>I will generate more information about {newArtist} soon! In the meantime, you can ask google.</p>
+        <a href={`https://www.google.com/search?q=${newArtist.replaceAll(' ', '+')}`} target='blank'>
+          Google {newArtist}
+        </a>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        &#169; by Clemens Leopold
       </p>
     </>
   )
